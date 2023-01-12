b148e02c41a34a1cb4483d868dd7a69722b50c25
AureKad <93446185+AureKad@users.noreply.github.com>
2023-01-12 04:11:57 +0100

    cuh

--- Memo/html/app/js/script.js
+++ Memo/html/app/js/script.js
@@ -1,26 +1,27 @@
 const books = document.getElementsByClassName('hero__list')[0];
 
-function addBook(book, url) {
+function addBook(book, url, desc) {
     let container = document.createElement('div');
-    let entry = document.createElement('li');
-    let close = document.createElement('button');
-    let container_name = document.createElement('div')
     let container_update = document.createElement('div')
+    let container_text = document.createElement('div')
+    let entry = document.createElement('li');
+    let remove = document.createElement('button');
     let label = document.createElement('label');
     let input_url = document.createElement('input');
     let input_button = document.createElement('button');
+    let textarea_desc = document.createElement('div');
 
     container.className = 'hero__list__container';
     entry.className = 'hero__list__elem';
-    close.className = 'hero__list__remove';
+    remove.className = 'hero__list__elem__remove';
 
-    close.innerText = 'X'
-    close.addEventListener('click', function(e) {
+    remove.innerText = 'X'
+    remove.addEventListener('click', function(e) {
       // Delete from localstorage
       var result = confirm("Are you sure you want to delete the book?");
       if (result) {
         let data = JSON.parse(localStorage.getItem('books'));
-        let index = getIndexOfBook(data, book, url);
+        let index = getIndexOfBook(data, book, url, desc);
         data.splice(index, 1);
     
         localStorage.setItem('books', JSON.stringify(data));
@@ -33,8 +34,8 @@ function addBook(book, url) {
         let new_url = input_url.value
         if (new_url != "" || (new_url == "" && url != "")) {
             let data = JSON.parse(localStorage.getItem('books'));
-            index = getIndexOfBook(data, book, url);
-            data.splice(index, 1, [book, new_url]);
+            index = getIndexOfBook(data, book, url, desc);
+            data.splice(index, 1, [book, new_url, desc]);
             localStorage.setItem('books', JSON.stringify(data));
             entry.onclick = function () {openInNewTab(new_url);};
             alert("Url updated!");
@@ -46,8 +47,8 @@ function addBook(book, url) {
     input_button.textContent = 'Update';
     input_button.className = 'hero__list__elem__update';
     input_url.className = 'hero__list__elem__text';
-    container_update.className = 'hero__list__elem__container';
-    container_name.className = 'hero__list__namecontainer'
+    container_update.className = 'hero__list__elem__updatecontainer';
+    container_text.className = 'hero__list__elem__textcontainer'
 
     entry.appendChild(document.createTextNode(book));
     if (url != "") {
@@ -55,12 +56,34 @@ function addBook(book, url) {
         input_url.value = url
     }
 
-    container_name.appendChild(close);
-    container_name.appendChild(entry);
-    container.appendChild(container_name)
-    container_update.appendChild(label);
+    textarea_desc.addEventListener('input', function(e) {
+        let data = JSON.parse(localStorage.getItem('books'));
+        index = getIndexOfBook(data, book, url, desc);
+        data.splice(index, 1, [book, url, textarea_desc.innerHTML]);
+        localStorage.setItem('books', JSON.stringify(data));
+    })
+    textarea_desc.addEventListener('focusout', function(e) {
+        if (textarea_desc.innerHTML=="") {
+            textarea_desc.innerHTML = 'Write something here!'
+        }
+    })
+    textarea_desc.contentEditable = true
+
+    if (desc == "") {
+        textarea_desc.innerHTML = 'Write something here!'
+    } else {
+        textarea_desc.innerHTML = desc
+    }
+
+    textarea_desc.className = 'hero__list__elem__desc'
+
+    container_update.appendChild(label);                                       
     container_update.appendChild(input_url);
     container_update.appendChild(input_button);
+    container_text.appendChild(remove)
+    container_text.appendChild(entry)
+    container_text.appendChild(textarea_desc)
+    container.appendChild(container_text)
     container.appendChild(container_update)
     books.appendChild(container);
 }
@@ -81,7 +104,7 @@ function addBook_help() {
     input = document.getElementById('addbook_input')
     let data = JSON.parse(localStorage.getItem('books'));
     if (input.value!="" && !data.map(d => d[0]).map(book => book.toLowerCase()).includes(input.value.toLowerCase())) {
-        addBook(input.value,'');
+        addBook(input.value,'','');
         data.splice(data.length, 0, [input.value, ""]);
         localStorage.setItem('books', JSON.stringify(data));
         input.value = "";
@@ -107,14 +130,38 @@ function create_list() {
     });
 }
 
+function search() {
+    input = document.getElementById('search_input').value.toLowerCase();
+    if (input != "") {
+        const data = JSON.parse(localStorage.getItem('books'));
+        let showBooks = []
+        data.forEach(element => {
+            if (element[0].toLowerCase().includes(input)) {
+                showBooks.push(element)
+            }
+        });
+        delete_list()
+        showBooks.forEach(element => {
+            addBook(...element)
+        })
+    } else {
+        delete_list()
+        create_list()
+    }
+}
+
+function reset() {
+    delete_list()
+    create_list()
+    input = document.getElementById('search_input');
+    input.value = ""
+    input = document.getElementById('addbook_input');
+    input.value = ""
+}
 
 
-window.addEventListener('load', function()
-{
-    const data = JSON.parse(localStorage.getItem('books'));
-    data.forEach(element => {
-        addBook(...element)
-    });
+window.addEventListener('load', function(){
+    create_list()
 });
 
 /*const data = [["Apocalypse Redux", ""], ["Bastion", ""], ["Chainsawman", "https://www.chainsaw-man-manga.online/manga/chainsaw-man-chapter-112/"], 
