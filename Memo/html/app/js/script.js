f2b973d6683f53092a2c2655cad9ac597908b5da
AureKad <93446185+AureKad@users.noreply.github.com>
2023-01-15 17:42:38 +0100

    ungabunga

--- Memo/html/app/js/script.js
+++ Memo/html/app/js/script.js
@@ -1,19 +1,94 @@
 const books = document.getElementsByClassName('hero__list')[0];
 let site = window.location.url
 
-function addBook(book, url, desc, site) {
+function addBook(book, url, desc, site, bm, fav) {
     let container = document.createElement('div');
     let container_update = document.createElement('div')
     let container_text = document.createElement('div')
-    let entry = document.createElement('li');
-    let remove = document.createElement('button');
-    let label = document.createElement('label');
-    let input_url = document.createElement('input');
-    let input_button = document.createElement('button');
-    let textarea_desc = document.createElement('div');
 
     container.className = 'hero__list__container';
-    entry.className = 'hero__list__elem';
+    container_update.className = 'hero__list__elem__updatecontainer';
+    container_text.className = 'hero__list__elem__textcontainer';
+
+    container_bm = addBook_bookmark(book, url, desc, site, bm, fav)
+    container_star = addBook_favstar(book, url, desc, site, bm, fav)
+    remove = addBook_remove(book, url, desc, site, bm, fav)
+    let {input_button, input_url, label} = addBook_update(book, url, desc, site, bm, fav)
+    entry = addBook_entry(book, url, input_url)
+    textarea_desc = addBook_desc(book, url, desc, site, bm, fav)
+
+
+    container_update.appendChild(label);                                       
+    container_update.appendChild(input_url);
+    container_update.appendChild(input_button);
+    container_text.appendChild(container_bm)
+    container_text.appendChild(container_star)
+    container_text.appendChild(remove)
+    container_text.appendChild(entry)
+    container_text.appendChild(textarea_desc)
+    container.appendChild(container_text)
+    container.appendChild(container_update)
+    books.appendChild(container);
+}
+
+function addBook_bookmark(book, url, desc, site, bm, fav) {
+    let container_bm = document.createElement('div');
+    let outline_bm = document.createElement('span');
+    let fill_bm = document.createElement('span');
+
+    container_bm.className = 'icon-wrap'
+    outline_bm.className = 'bookmark bi bi-bookmark-star'
+    fill_bm.className = 'bookmark bi bi-bookmark-star-fill fill'
+    container_bm.title = "Bookmark this!"
+
+    fill_bm.style.opacity = bm ? 1 : 0;
+    container_bm.addEventListener('click', function(e) {
+        let data = JSON.parse(localStorage.getItem('books'));
+        index = getIndexOfBook(data, book, url, desc, site, bm, fav);
+        datapoint = data[index]
+        datapoint[4] = !datapoint[4]
+        data.splice(index, 1, datapoint);
+        localStorage.setItem('books', JSON.stringify(data));
+        fill_bm.style.opacity = datapoint[4] ? 1 : 0;
+        
+        delete_list()
+        create_list()
+    })
+    container_bm.appendChild(outline_bm)
+    container_bm.appendChild(fill_bm)
+    return container_bm
+}
+
+function addBook_favstar(book, url, desc, site, bm, fav) {
+    let container_star = document.createElement('div');
+    let outline_star = document.createElement('span');
+    let fill_star = document.createElement('span');
+
+    container_star.className = 'icon-wrap'
+    outline_star.className = 'fav-star bi bi-star'
+    fill_star.className = 'fav-star bi bi-star-fill fill'
+    container_star.title = "Mark as Favorite!"
+
+    fill_star.style.opacity = fav ? 1 : 0;
+    container_star.addEventListener('click', function(e) {
+        let data = JSON.parse(localStorage.getItem('books'));
+        index = getIndexOfBook(data, book, url, desc, site, bm, fav);
+        datapoint = data[index]
+        datapoint[5] = !datapoint[5]
+        data.splice(index, 1, datapoint);
+        localStorage.setItem('books', JSON.stringify(data));
+        fill_star.style.opacity = datapoint[5] ? 1 : 0;
+        
+        delete_list()
+        create_list()
+    })
+    container_star.appendChild(outline_star)
+    container_star.appendChild(fill_star)
+    return container_star
+}
+
+function addBook_remove(book, url, desc, site, bm, fav) {
+    let remove = document.createElement('button');
     remove.className = 'hero__list__elem__remove';
 
     remove.innerText = 'X'
@@ -22,7 +97,7 @@ function addBook(book, url, desc, site) {
       var result = confirm("Are you sure you want to delete the book?");
       if (result) {
         let data = JSON.parse(localStorage.getItem('books'));
-        let index = getIndexOfBook(data, book, url, desc, site);
+        let index = getIndexOfBook(data, book, url, desc, site, bm, fav);
         data.splice(index, 1);
     
         localStorage.setItem('books', JSON.stringify(data));
@@ -30,15 +105,23 @@ function addBook(book, url, desc, site) {
         this.parentElement.parentElement.remove();
        }
     });
+    return remove
+}
+
+function addBook_update(book, url, desc, site, bm, fav) {
+    let label = document.createElement('label');
+    let input_url = document.createElement('input');
+    let input_button = document.createElement('button');
 
     input_button.addEventListener('click', function(e) {
         let new_url = input_url.value
         if (new_url != "" || (new_url == "" && url != "")) {
             let data = JSON.parse(localStorage.getItem('books'));
-            index = getIndexOfBook(data, book, url, desc, site);
-            data.splice(index, 1, [book, new_url, desc, site]);
+            index = getIndexOfBook(data, book, url, desc, site, bm, fav);
+            data.splice(index, 1, [book, new_url, desc, site, bm, fav]);
             localStorage.setItem('books', JSON.stringify(data));
             entry.onclick = function () {openInNewTab(new_url);};
+            url = new_url
             alert("Url updated!");
         }
     });
@@ -48,19 +131,28 @@ function addBook(book, url, desc, site) {
     input_button.textContent = 'Update';
     input_button.className = 'hero__list__elem__update';
     input_url.className = 'hero__list__elem__text';
-    container_update.className = 'hero__list__elem__updatecontainer';
-    container_text.className = 'hero__list__elem__textcontainer'
+    return {input_button, input_url, label}
+}
 
+function addBook_entry(book, url, input_url) {
+    let entry = document.createElement('li');
+    entry.className = 'hero__list__elem';
     entry.appendChild(document.createTextNode(book));
     if (url != "") {
         entry.onclick = function () {openInNewTab(url);};
         input_url.value = url
     }
+    return entry
+}
+
+function addBook_desc(book, url, desc, site, bm, fav) {
+    let textarea_desc = document.createElement('div');
+    textarea_desc.className = 'hero__list__elem__desc'
 
     textarea_desc.addEventListener('input', function(e) {
         let data = JSON.parse(localStorage.getItem('books'));
-        index = getIndexOfBook(data, book, url, desc, site);
-        data.splice(index, 1, [book, url, textarea_desc.innerHTML, site]);
+        index = getIndexOfBook(data, book, url, desc, site, bm, fav);
+        data.splice(index, 1, [book, url, textarea_desc.innerHTML, site, bm, fav]);
         localStorage.setItem('books', JSON.stringify(data));
     })
     textarea_desc.addEventListener('focusout', function(e) {
@@ -75,18 +167,7 @@ function addBook(book, url, desc, site) {
     } else {
         textarea_desc.innerHTML = desc
     }
-
-    textarea_desc.className = 'hero__list__elem__desc'
-
-    container_update.appendChild(label);                                       
-    container_update.appendChild(input_url);
-    container_update.appendChild(input_button);
-    container_text.appendChild(remove)
-    container_text.appendChild(entry)
-    container_text.appendChild(textarea_desc)
-    container.appendChild(container_text)
-    container.appendChild(container_update)
-    books.appendChild(container);
+    return textarea_desc
 }
 
 function openInNewTab(url) {
@@ -112,7 +193,7 @@ function addBook_help() {
     });
     if (input.value!="" && (!new_data.map(d => d[0]).map(book => book.toLowerCase()).includes(input.value.toLowerCase()))) {
         addBook(input.value,'','', site);
-        data.splice(data.length, 0, [input.value, '', '', site]);
+        data.splice(data.length, 0, [input.value, '', '', site, false, false]);
         localStorage.setItem('books', JSON.stringify(data));
         input.value = "";
     }
@@ -133,7 +214,22 @@ function delete_list() {
 function create_list() {
     const data = JSON.parse(localStorage.getItem('books'));
     data.forEach(element => {
-        if (element[3] == site) {
+        if (element[3] == site && element[4] && element[5]) {
+            addBook(...element)
+        }
+    });
+    data.forEach(element => {
+        if (element[3] == site && element[4] && !element[5]) {
+            addBook(...element)
+        }
+    });
+    data.forEach(element => {
+        if (element[3] == site && !element[4] && element[5]) {
+            addBook(...element)
+        }
+    });
+    data.forEach(element => {
+        if (element[3] == site && !element[4] && !element[5]) {
             addBook(...element)
         }
     });
@@ -173,6 +269,9 @@ function reset() {
 
 window.addEventListener('load', function(){
     site = window.location.href.split("/").at(-1)
+    if (site=="") {
+        site = 'index.html'
+    }
     create_list()
 });
 
@@ -196,9 +295,11 @@ function save_file() {
     link.click();
     URL.revokeObjectURL(link.href);
 }
-
+const data = JSON.parse(localStorage.getItem('books'));
+data.forEach(e => {
+})
 /*
-const data = [["A Journey of Black and Red","https://www.royalroad.com/fiction/26675/a-journey-of-black-and-red/chapter/398080/13-homeward","I am so uwu omg","webnovel.html"],
+const data = [["A Journey of Black and Red","https://www.royalroad.com/fiction/26675/a-journey-of-black-and-red/chapter/398080/13-homeward","I am so uwu omg","webnovels.html"],
 ["Apocalypse Redux","","Write","webnovels.html"],
 ["Azarinth Healer","","","webnovels.html"],
 ["Bastion","","","index.html"],
@@ -209,7 +310,7 @@ const data = [["A Journey of Black and Red","https://www.royalroad.com/fiction/2
 ["Iron Prince","","","index.html"],
 ["Lord of the Mysteries","https://boxnovel.com/novel/lord-of-the-mysteries-boxnovel/chapter-27/","","webnovels.html"],
 ["Magical Girl Gunslinger","https://www.royalroad.com/fiction/48402/magical-girl-gunslinger","","webnovels.html"],
-["Mistborn","","","books.html"],
+["Mistborn","","","index.html"],
 ["One Punch Man","https://mangarockteam.com/manga/onepunch-man/chapter-176/","","manga.html"],
 ["Salvos","https://www.royalroad.com/fiction/37438/hellprince…ster-evolution-litrpg/chapter/1076288/480-regroup","","webnovels.html"],
 ["Speedrun the Multiverse","","","webnovels.html"],
